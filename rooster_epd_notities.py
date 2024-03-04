00896727064a8b08cec47e90d792a09f3560f142
Duisterethomas <duisterethomas@gmail.com>
2024-03-04 12:19:35 +0100

    Add save button disable to notities

--- rooster_epd_notities.py
+++ rooster_epd_notities.py
@@ -1,6 +1,7 @@
+from copy import deepcopy
 from pickle import dump
 
-from PySide6.QtWidgets import QDialog
+from PySide6.QtWidgets import QDialog, QDialogButtonBox
 
 from rooster_epd_ui import Ui_Rooster_epd_notities
 
@@ -22,16 +23,31 @@ class notitiesWindow(QDialog, Ui_Rooster_epd_notities):
         self.vrijdag.setText(save_dict["notities"][4])
         self.zaterdag.setText(save_dict["notities"][5])
         self.zondag.setText(save_dict["notities"][6])
+        
+        # Add a check if the save button should be disabled
+        self.maandag.textChanged.connect(self.checkSaveDisable)
+        self.dinsdag.textChanged.connect(self.checkSaveDisable)
+        self.woensdag.textChanged.connect(self.checkSaveDisable)
+        self.donderdag.textChanged.connect(self.checkSaveDisable)
+        self.vrijdag.textChanged.connect(self.checkSaveDisable)
+        self.zaterdag.textChanged.connect(self.checkSaveDisable)
+        self.zondag.textChanged.connect(self.checkSaveDisable)
+        
+        self.checkSaveDisable()
 
+    def checkSaveDisable(self):
+        self.new_notities = (self.maandag.text(),
+                             self.dinsdag.text(),
+                             self.woensdag.text(),
+                             self.donderdag.text(),
+                             self.vrijdag.text(),
+                             self.zaterdag.text(),
+                             self.zondag.text())
+        
+        self.buttonBox.button(QDialogButtonBox.Save).setDisabled(self.new_notities == self.save_dict["notities"])
+    
     # Save the notities
     def saveNotities(self):
-        self.save_dict["notities"] = (self.maandag.text(),
-                                      self.dinsdag.text(),
-                                      self.woensdag.text(),
-                                      self.donderdag.text(),
-                                      self.vrijdag.text(),
-                                      self.zaterdag.text(),
-                                      self.zondag.text())
-
+        self.save_dict["notities"] = deepcopy(self.new_notities)
         with open("rooster-epd.data", "wb") as save_file:
             dump(self.save_dict, save_file)
